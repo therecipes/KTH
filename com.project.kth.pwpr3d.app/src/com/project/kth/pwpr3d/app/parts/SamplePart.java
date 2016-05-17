ea40d9082ad36de56074b60c91262532df2bf085
chisoftltd <chisoftltd@gmail.com>
2016-05-17 16:38:28 +0200

    update

--- com.project.kth.pwpr3d.app/src/com/project/kth/pwpr3d/app/parts/SamplePart.java
+++ com.project.kth.pwpr3d.app/src/com/project/kth/pwpr3d/app/parts/SamplePart.java
@@ -14,33 +14,19 @@ package com.project.kth.pwpr3d.app.parts;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
-import org.eclipse.draw2d.LightweightSystem;
-import org.eclipse.draw2d.MarginBorder;
-import org.eclipse.draw2d.parts.ScrollableThumbnail;
-import org.eclipse.draw2d.parts.Thumbnail;
 import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.e4.ui.di.Persist;
 import org.eclipse.e4.ui.model.application.ui.MDirtyable;
-import org.eclipse.gef.LayerConstants;
-import org.eclipse.gef.editparts.ScalableFreeformRootEditPart;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.ISelectionChangedListener;
-import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Text;
-import org.eclipse.ui.IPageLayout;
-import org.eclipse.ui.part.Page;
-import org.eclipse.ui.views.contentoutline.IContentOutlinePage;
 
-public class SamplePart extends Page implements IContentOutlinePage {
+public class SamplePart {
 
 	private Text txtInput;
 	private TableViewer tableViewer;
@@ -48,29 +34,8 @@ public class SamplePart extends Page implements IContentOutlinePage {
 	@Inject
 	private MDirtyable dirty;
 
-	private Canvas overview;
-	private ScalableFreeformRootEditPart rootEditPart;
-	private Thumbnail thumbnail;
-
-	public SamplePart(ScalableFreeformRootEditPart rootEditPart) {
-		super();
-		this.rootEditPart = rootEditPart;
-	}
-
-	public void addSelectionChangedListener(ISelectionChangedListener listener) {
-	}
-
 	@PostConstruct
 	public void createComposite(Composite parent) {
-
-		overview = new Canvas(parent, SWT.NONE);
-		LightweightSystem lws = new LightweightSystem(overview);
-		thumbnail = new ScrollableThumbnail((Viewport) rootEditPart.getFigure());
-		thumbnail.setBorder(new MarginBorder(3));
-		thumbnail.setSource(rootEditPart
-				.getLayer(LayerConstants.PRINTABLE_LAYERS));
-		lws.setContents(thumbnail);
-
 		parent.setLayout(new GridLayout(1, false));
 
 		txtInput = new Text(parent, SWT.BORDER);
@@ -96,45 +61,10 @@ public class SamplePart extends Page implements IContentOutlinePage {
 	@Focus
 	public void setFocus() {
 		tableViewer.getTable().setFocus();
-		if (getControl() != null)
-			getControl().setFocus();
 	}
 
 	@Persist
 	public void save() {
 		dirty.setDirty(false);
 	}
-
-	@Override
-	public ISelection getSelection() {
-		return StructuredSelection.EMPTY;
-	}
-
-	@Override
-	public void removeSelectionChangedListener(
-			ISelectionChangedListener listener) {
-	}
-
-	@Override
-	public void setSelection(ISelection selection) {
-	}
-
-	@Override
-	public void createControl(Composite arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public Control getControl() {
-		// TODO Auto-generated method stub
-		return overview;
-	}
-
-	public void dispose() {
-		if (null != thumbnail)
-			thumbnail.deactivate();
-		super.dispose();
-	}
-
 }
