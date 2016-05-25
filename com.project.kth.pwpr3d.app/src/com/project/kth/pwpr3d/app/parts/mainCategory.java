dc7d42ca8688bc044a67835f8fdec167d1108bce
DrAyettey <Ayettey@10.211.55.4>
2016-05-25 15:52:33 +0200

    New update

---
+++ com.project.kth.pwpr3d.app/src/com/project/kth/pwpr3d/app/parts/mainCategory.java
@@ -0,0 +1,30 @@
+package com.project.kth.pwpr3d.app.parts;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class mainCategory {
+//Class for parent Node for Tree
+private String name;
+private int sort;
+//Array List For storing List of Child Nodes
+private List<MyFiles> files = new ArrayList<MyFiles>();
+public String getName() {
+return name;
+}
+public void setName(String name) {
+this.name = name;
+}
+public int getSort(){
+
+return sort;
+}
+public void setSort(int sort) {
+this.sort = sort;
+}
+public List<MyFiles> getFiles(){
+return files;
+}
+}
